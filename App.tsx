065ecb97d01896690a3dc9dc79fe4ccc62f19590
Aureo-Bueno <aureoalexandre@outlook.com>
2023-01-31 19:15:43 -0500

    feat: add template project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Card } from '@rneui/themed';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+       <Card>
+        <Text>Teste</Text>
+        <Button 
+          size='md'
+          type='outline'
+        >
+          Teste
+        </Button>
+       </Card>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
