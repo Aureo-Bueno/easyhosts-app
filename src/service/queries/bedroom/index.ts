f7ec90ae588ab907422d84a4824c1de0229262d3
LuizRomao02 <luizromao7@outlook.com>
2023-05-22 14:12:04 -0300

    feat: settings axios bedroom

--- src/service/queries/bedroom/index.ts
+++ src/service/queries/bedroom/index.ts
@@ -1,11 +1,42 @@
 import { useQuery } from 'react-query';
 import { AxiosError } from 'axios';
-import { useGet } from './axios';
+import { axiosClient } from '../../';
 
-interface BedroomDTO {
+export interface IBedroom {
   id: number,
   name: string,
   number: number
 }
 
-export const useGetBedroom = () => useQuery<BedroomDTO[], AxiosError>('getBedrooms', useGet);
+export const useGet = async () => {
+  try {
+    const response = await axiosClient.get(`/api/easyhosts/Bedroom`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const usePost = async (name: string, number: number) => {
+  const response = await axiosClient.post(`/api/easyhosts/Bedroom`, {name, number});
+  return response.data;
+}
+
+export const useGetId = async (id: string) => {
+  const response = await axiosClient.get(`/api/easyhosts/Bedroom/${id}`);
+  return response.data;
+}
+
+export const useUpdate = async (name: string, number: number) => {
+  const response = await axiosClient.put(`/api/easyhosts/Bedroom/`, {name, number});
+  return response.data;
+}
+
+export const useDelete = async (id: string) => {
+  const response = await axiosClient.delete(`/api/easyhosts/Bedroom/${id}`);
+  return response.data;
+}
+
+
+export const useGetBedroom = () => useQuery<IBedroom[], AxiosError>('getBedrooms', useGet);
