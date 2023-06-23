5b9baac4f4411e1e7683719fbff8d92ad60520c4
Aure-Bueno <aureoalexandre@outlook.com>
2023-06-22 22:09:16 -0300

    feat: add users in list order service

--- src/screens/ListOrderService/index.tsx
+++ src/screens/ListOrderService/index.tsx
@@ -1,21 +1,23 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
 import { useUpdateStatusOrderService } from '../../service/mutations/orderServices/services/useUpdateStatusOrderService';
 import { useGetOrderServiceAll } from '../../service/queries/orderServices';
 import { IOrderServicesResponse } from '../../service/queries/orderServices/@types';
 import * as S from './styles';
-import { ActivityIndicator, FlatList, Text, TouchableOpacity } from 'react-native';
+import { FlatList, Text, TouchableOpacity } from 'react-native';
 import { AuthContext } from '../../context/AuthContext';
 import Modal from '../../components/Modal';
 import { Card, Divider } from '@rneui/base';
 import { useGetBookingId } from '../../service/queries/booking';
 
 function ListOrderServices() {
-  const [userBookingId, setUserBookingId] = useState<string>();
-  const { data: dataListOrderService } = useGetOrderServiceAll();
-  const { mutate } = useUpdateStatusOrderService();
-  const { user } = useContext(AuthContext);
+  const [userBookingId, setUserBookingId] = useState<string>('');
   const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  const { user } = useContext(AuthContext);
+
+  const { mutate } = useUpdateStatusOrderService();
+  const { data: dataListOrderService } = useGetOrderServiceAll();
   const { data: bookingByUser } = useGetBookingId(userBookingId);
 
   const handleCloseModal = () => {
@@ -23,7 +25,9 @@ function ListOrderServices() {
   };
 
   const renderItemStatusService = ({ item }: { item: IOrderServicesResponse }) => {
-    const bookingData = bookingByUser?.find((booking) => booking.userId === item.userId);
+    setUserBookingId(item.userId);
+    const teste = bookingByUser?.find((test) => test.userId === item.userId);
+
     const handleAlterStatusOrderService = () => {
       mutate(
         {
@@ -45,25 +49,30 @@ function ListOrderServices() {
       );
     };
     return (
-      <Card>
-        <S.TextStatus>
-          Descrição: {item.description}
-          {item.status === 1 && (
-            <TouchableOpacity onPress={handleAlterStatusOrderService}>
-              <S.Icon name="md-checkmark-circle" size={24} color="#039900" />
-            </TouchableOpacity>
-          )}
-          {item.status === 2 && <S.Icon name="refresh-circle-outline" size={24} color="#ff9d00" />}
-          {item.status === 3 && <S.Icon name="md-checkmark-circle" size={24} color="green" />}
-          {item.status === 4 && <S.Icon name="close-outline" size={24} color="#990000" />}
-        </S.TextStatus>
-        <S.TextStatus>Nome do Hóspede: {bookingData?.user.userName}</S.TextStatus>
-        <S.TextStatus>Número do quarto: {bookingData?.bedroom.number}</S.TextStatus>
+      <TouchableOpacity onPress={handleAlterStatusOrderService}>
+        <Card>
+          <Divider width={1} />
+          <S.TextStatus>
+            Descrição: {item.description}
+            {item.status === 1 && <S.Icon name="md-checkmark-circle" size={24} color="#039900" />}
+            {item.status === 2 && (
+              <S.Icon name="refresh-circle-outline" size={24} color="#ff9d00" />
+            )}
+            {item.status === 3 && <S.Icon name="md-checkmark-circle" size={24} color="green" />}
+            {item.status === 4 && <S.Icon name="close-outline" size={24} color="#990000" />}
+          </S.TextStatus>
+          <S.TextStatus>Nome do Hóspede: {teste?.user.userName} </S.TextStatus>
+          <S.TextStatus>Número do quarto: {teste?.bedroom.number}</S.TextStatus>
 
-        <Divider width={1} />
-      </Card>
+          <Divider width={1} />
+        </Card>
+      </TouchableOpacity>
     );
   };
+
+  useEffect(() => {
+    renderItemStatusService
+  }, [renderItemStatusService, userBookingId, dataListOrderService])
 
   return (
     <S.Container>
